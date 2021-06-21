a434578e1b470fdcc77efd6040ba139ff1987215
rinderomna <rinder_omna@outlook.com>
2021-06-21 11:57:16 -0300

    livro corrompido

---
+++ ICC/Bloco2/11-LivroCorrompido/livro.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *read_line(void);
+
+int main() {
+  int n_lines;
+
+  scanf("%d", &n_lines);
+  
+  getchar(); //Consumes '\n'
+  
+  char *lines[n_lines];
+
+  for (int i = 0; i < n_lines; i++)
+    lines[i] = read_line();
+
+  int v;
+
+  scanf("%d", &v);
+
+  int true_lines[v];
+
+  for (int i = 0; i < v; i++)
+    scanf("%d", &true_lines[i]);
+
+  for (int i = 0; i < v; i++)
+    printf("%s\n", lines[true_lines[i]]);
+
+  return 0;
+}
+
+char *read_line(void) {
+  char *line = NULL;
+  char c;
+  int n_chars = 0;
+  
+  do {
+    c = getchar();
+    
+    n_chars++;
+    line = (char *) realloc(line, n_chars);
+
+    if (c != '\n')
+      *(line + n_chars - 1) = c;
+    else
+      *(line + n_chars - 1) = '\0';
+
+  } while (c != '\n');
+
+  return line;
+}
---
+++ ICC/Bloco2/11-LivroCorrompido/testes/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 21/06/2021 11:36:07 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [Bloco 2 - 03] Livro Corrompido
