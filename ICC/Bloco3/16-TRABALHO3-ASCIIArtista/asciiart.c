40574c4854e101251d9dbe90a82783aa7f522711
rinderomna <rinder_omna@outlook.com>
2021-07-22 02:59:28 -0300

    ascii art com funcao de concatenar strings modificado

--- ICC/Bloco3/16-TRABALHO3-ASCIIArtista/asciiart.c
+++ ICC/Bloco3/16-TRABALHO3-ASCIIArtista/asciiart.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-char *read_line(FILE *stream);
+char *read_line(FILE *stream, bool *has_EOF);
 char *str_concatenate(const char *str_1, const char *str_2);
 void enquadra_arte(
 	char *nome_do_art_file,
@@ -12,16 +13,27 @@ void enquadra_arte(
 
 int main(void) {
   FILE *art_file = NULL;
+  bool has_EOF = false;
   char *art_name;
   char *path_to_art;
 
-  art_name = read_line(stdin);
+  art_name = read_line(stdin, &has_EOF);
   path_to_art = str_concatenate("artes/", art_name);
 
   printf("%s\n", art_name);
   printf("%s\n", path_to_art);
 
-  fopen(path_to_art, "r");
+  //art_file = fopen(path_to_art, "r");
+
+  // Inicio do codigo
+
+  char **art_matrix;
+
+
+
+  // Final do codigo
+
+  //fclose(art_file);
 
   free(art_name);
   free(path_to_art);
@@ -29,7 +41,7 @@ int main(void) {
   return 0;
 }
 
-char *read_line(FILE *stream) {
+char *read_line(FILE *stream, bool *has_EOF) {
   char *line = NULL;
   char c;
   int n_chars = 0;
@@ -40,10 +52,16 @@ char *read_line(FILE *stream) {
     n_chars++;
     line = (char *) realloc(line, n_chars);
 
-    if (c != '\n' && c != EOF)
-      *(line + n_chars - 1) = c;
-    else
+    if (c == '\n') {
       *(line + n_chars - 1) = '\0';
+      *has_EOF = false;
+    } else if (c ==  EOF) {
+      *(line + n_chars - 1) = '\0';
+      *has_EOF = true;
+    } else {
+      *(line + n_chars - 1) = c;
+      *has_EOF = false;
+    }
 
   } while (c != '\n' && c != EOF);
 
