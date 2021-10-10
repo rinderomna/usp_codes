b146fc4e4efeedf6c1ccf566db87097531424fb1
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2021-10-09 21:09:52 -0300

    delimitadores

--- C/2021-02/ALG/Exercicios/5-SequenciaBalanceada/main.c
+++ C/2021-02/ALG/Exercicios/5-SequenciaBalanceada/main.c
@@ -19,78 +19,78 @@ enum {
     OUTRO
 };
 
-int tipo_de_clausura(char clausura);
+int tipo_de_delimitador(char delimitador);
 
 int main() {
-    pilha_t *pilha_de_clausuras = criar_pilha();
+    pilha_t *pilha_de_delimitadores = criar_pilha();
 
-    char clausura = 0;
+    char delimitador = 0;
 
     int balanceado = 1;
     
     do {
-        clausura = getchar();
+        delimitador = getchar();
 
-        if (clausura == '\n' || clausura == EOF) {
-            if (balanceado && get_tamanho_da_pilha(pilha_de_clausuras) == 0) {
+        if (delimitador == '\n' || delimitador == EOF) {
+            if (balanceado && get_tamanho_da_pilha(pilha_de_delimitadores) == 0) {
                 printf("BALANCEADO");
             } else {   
                 printf("NÃO BALANCEADO");
                 balanceado = 1;
             }
 
-            if (clausura != EOF) {
+            if (delimitador != EOF) {
                 printf("\n");
             }
 
-            esvaziar_pilha(pilha_de_clausuras);
-        } else if (clausura != ' ') {
-            int tipo_da_clausura = tipo_de_clausura(clausura);
+            esvaziar_pilha(pilha_de_delimitadores);
+        } else if (delimitador != ' ') {
+            int tipo_da_delimitador = tipo_de_delimitador(delimitador);
 
-            if (tipo_da_clausura == ABERTURA) {
-                push(pilha_de_clausuras, clausura);
-            } else if (tipo_da_clausura == FECHAMENTO) {
-                elem_t ultima_clausura = ' ';
-                int status_da_retirada = pop(pilha_de_clausuras, &ultima_clausura);   
+            if (tipo_da_delimitador == ABERTURA) {
+                push(pilha_de_delimitadores, delimitador);
+            } else if (tipo_da_delimitador == FECHAMENTO) {
+                elem_t ultima_delimitador = ' ';
+                int status_da_retirada = pop(pilha_de_delimitadores, &ultima_delimitador);   
                 if (status_da_retirada == FAIL ||
-                    (ultima_clausura != '[' || clausura != ']') &&
-                    (ultima_clausura != '(' || clausura != ')') &&
-                    (ultima_clausura != '{' || clausura != '}')
+                    (ultima_delimitador != '[' || delimitador != ']') &&
+                    (ultima_delimitador != '(' || delimitador != ')') &&
+                    (ultima_delimitador != '{' || delimitador != '}')
                 ) {
                     balanceado = 0;
                 }
-            } else if (tipo_da_clausura == ASPAS) {
-                char ultima_clausura = ' ';
-                int status_da_consulta = top(pilha_de_clausuras, &ultima_clausura);
+            } else if (tipo_da_delimitador == ASPAS) {
+                char ultima_delimitador = ' ';
+                int status_da_consulta = top(pilha_de_delimitadores, &ultima_delimitador);
 
-                if (status_da_consulta == FAIL || ultima_clausura != clausura) {
-                    push(pilha_de_clausuras, clausura);
+                if (status_da_consulta == FAIL || ultima_delimitador != delimitador) {
+                    push(pilha_de_delimitadores, delimitador);
                 } else {
-                    pop(pilha_de_clausuras, &ultima_clausura);
+                    pop(pilha_de_delimitadores, &ultima_delimitador);
                 }
             }
         }
-    } while (clausura != EOF);
+    } while (delimitador != EOF);
 
-    destruir_pilha(&pilha_de_clausuras);
+    destruir_pilha(&pilha_de_delimitadores);
 
     return 0;
 }
 
-int tipo_de_clausura(char clausura) {
+int tipo_de_delimitador(char delimitador) {
     if (
-        clausura == '[' ||
-        clausura == '(' ||
-        clausura == '{'
+        delimitador == '[' ||
+        delimitador == '(' ||
+        delimitador == '{'
     ) {
         return ABERTURA;
     } else if (
-        clausura == ']' ||
-        clausura == ')' ||
-        clausura == '}'
+        delimitador == ']' ||
+        delimitador == ')' ||
+        delimitador == '}'
     ) {
         return FECHAMENTO;
-    } else if (clausura == '"') {
+    } else if (delimitador == '"') {
         return ASPAS;
     } else {
         return OUTRO;
